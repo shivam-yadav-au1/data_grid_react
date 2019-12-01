3ebea34230b896013c1aff60539fb77daee13458
shivam-yadav-au1 <shivamcs0049@gmail.com>
2019-12-01 17:38:38 +0530

    data table

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './App.css';
+import DataTable from './Components/DataTable/index'
+
+class App extends React.Component {
+
+    constructor(props){
+      super(props);
+
+      let modal = {
+        headers:[
+          {title:"Id",accessor:"id",index:0},
+          {title:"Profile",accessor:"profile",width:80,index:1,cell:{
+            type:"image",
+            style:{
+              "width":"50px",
+            }
+          }},
+          {title:"Name",accessor:"name",width:300,index:2},
+          {title:"Age",accessor:"age",index:2},
+          {title:"Qualification",accessor:"qualification",index:4},
+          {title:"Rating",accessor:"rating",index:5,width:200,cell:row=>(
+            <div className="rating">
+                <div style={{
+                  background:"lightskyblue",
+                  textAlign:"center",
+                  height:"1.9em",
+                  width:(row/5)*201 +"px",
+                  margin:"3px 0 4px 0"
+                }}>
+                  {row}
+                </div>
+            </div>
+          )},
+        ],
+        data: [
+          { id: 1, name: "a", age: 29, qualification: "B.Com", rating: 3, profile: "https://png.icons8.com/nolan/50/000000/user.png" },
+          { id: 2, name: "b", age: 35, qualification: "B.Sc", rating: 5, profile: "https://png.icons8.com/nolan/50/000000/user.png" },
+          { id: 3, name: "c", age: 42, qualification: "B.E", rating: 3, profile: "https://png.icons8.com/nolan/50/000000/user.png" },
+        ]
+      }
+
+      
+      for(let i=4;i<=20;i++){
+        modal.data.push({
+          id:i,
+          name:"name" + i,
+          age:i+18,
+          qualification:"Graduate",
+          rating:(i%2 ? 3:4),
+          profile:"https://png.icons8.com/dotty/50/000000/cat-profile.png"
+        })
+      }
+      this.state = modal;
+    }
+
+  render(){
+    return (
+      <div className="App">
+            <DataTable className="data-table"
+              title="USER PROFILES"
+              keyField="id"
+              pagination={{
+                enabled:true,
+                pageLength:5,
+                type:"long"
+              }}
+              width="100%"
+              headers={this.state.headers}
+              data={this.state.data}
+              noData="No records !"
+            
+            />
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/Components/DataTable/index.js
@@ -0,0 +1,153 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './datatable.css'
+
+class DataTable extends React.Component{
+
+    constructor(props){
+        super(props)
+
+        this.state={
+            headers:props.headers,
+            data:props.data,
+            sortby:null,
+            descending:null
+        }
+        this.keyField = props.keyField || "id";
+        this.noData = props.noData || "No records found!";
+        this.width = props.width || "100%";
+    }
+
+    renderTableHeader = () =>{
+        let {headers} = this.state;
+        // headers.sort((a,b)=>{
+        //     if(a.index > b.index) return 1;
+        //     return -1;
+        // })
+
+        let headerView = headers.map((header,index)=>{
+            let title = header.title;
+            let cleanTitle = header.accessor;
+            let width = header.width;
+
+            if(this.state.sortby === index){
+                title += this.state.descending ? '\u2193':'\u2191';
+            }
+
+            return (
+                <th key={cleanTitle}
+                    ref = {(th)=>this.th = th}
+                    style = {{width:width}}
+                    data-col={cleanTitle}>
+                    <span className="header-cell" data-col={cleanTitle}>{title}</span>
+                </th>
+            )
+        })
+        return headerView;
+    }
+    renderContent = ()=>{
+        let {headers,data} = this.state;
+        let contentView = data.map((row,rowIdx)=>{
+            let id = row[this.keyField];
+            let tds = headers.map((header,index)=>{
+                let content = row[header.accessor]
+                let cell = header.cell;
+                if(cell){
+                    if(typeof(cell) === 'object'){
+                        if(cell.type === 'image' && content){
+                            content = <img style={cell.style} src={content}/>
+                        }
+                    }
+                    else if(typeof(cell) === 'function'){
+                        content = cell(content);
+                    }
+                }
+
+                return (
+                    <td key={index} data-id={id} data-row={rowIdx}>{content}</td>
+                )
+
+            });
+            return (
+                <tr>
+                    {tds}
+                </tr>
+            )
+        });
+        
+       return contentView;
+
+    }
+    renderNoData = () =>{
+        return(
+            <tr>
+                <td colSpan={this.props.headers.length}>
+                    {this.noData}
+                </td>
+            </tr>
+        )
+    }
+
+    onSort =(e)=>{
+        
+        let data = this.state.data.slice();//Give new array
+        let colIndex = ReactDOM.findDOMNode(e.target).parentNode.cellIndex;
+        console.log(ReactDOM.findDOMNode(e.target).parentNode.cellIndex);
+        let colTitle = e.target.dataset.col;
+        let descending = !this.state.descending;
+        
+        data.sort((a,b)=>{
+            let sortVal = 0;
+            if(a[colTitle] < b[colTitle]){
+                sortVal = -1
+            }else if(a[colTitle] > b[colTitle]){
+                sortVal = 1;
+            }
+
+            if(descending){
+                sortVal = sortVal * -1;
+            }
+            return sortVal;
+        })
+
+        this.setState({
+            data,
+            sortby:colIndex,
+            descending
+        })
+    }
+
+    renderTable = ()=>{
+        let title = this.props.title || "DataTable";
+        let headerView = this.renderTableHeader();
+        let contentView = this.state.data.length > 0 ? this.renderContent() 
+                          : this.renderNoData();
+
+        return (
+            <table className="data-inner-table">
+                <caption className="data-table-caption">
+                    {title}
+                </caption>
+                <thead onClick={this.onSort}>
+                    <tr>
+                        {headerView}
+                    </tr> 
+                </thead>
+                <tbody>
+                    
+                    {contentView}
+                </tbody>
+            </table>
+        )
+    }
+
+    render(){
+        return (
+           <div className={this.props.className}>
+               {this.renderTable()}
+           </div>
+        )
+    }
+}
+
+export default DataTable;
